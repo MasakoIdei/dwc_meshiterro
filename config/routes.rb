d4e179a76ec77448eda353870e21b49788d9eeef
MasakoIdei <tarte.groseilles@gamil.com>
2021-05-13 13:39:12 +0000

    [add]4-4 いいね機能のテーブルとモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
  devise_for :users
  root 'homes#top'
  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+
+  # いいね機能
+  resource :favorites, only: [:create, :destroy]
+  
+  # コメントの作成、削除
   resources :post_comments, only: [:create, :destroy]
  end
 
